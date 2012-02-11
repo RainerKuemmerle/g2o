33e87709b1088248bbbfc37017c36adbdae02050
Rainer Kuemmerle <kuemmerl@informatik.uni-freiburg.de>
2012-02-11 21:09:31 +0100

    recover Eigen 3.0.5 patches

--- EXTERNAL/eigen3/test/geo_quaternion.cpp
+++ EXTERNAL/eigen3/test/geo_quaternion.cpp
@@ -155,7 +155,6 @@ template<typename Scalar, int Options> void quaternion(void)
   // test bug 369 - improper alignment.
   Quaternionx *q = new Quaternionx;
   delete q;
-<<<<<<< HEAD
 
   q1 = AngleAxisx(a, v0.normalized());
   q2 = AngleAxisx(b, v1.normalized());
