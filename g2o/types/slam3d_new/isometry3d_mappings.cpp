2cba86dff87d7363f1a19fa449670b10dfa5549d
Giorgio Grisetti <grisetti@informatik.uni-freiburg.de>
2012-05-16 15:47:30 +0200

    fixed special functions to generate the gradients for edge_se3 new

--- g2o/types/slam3d_new/isometry3d_mappings.cpp
+++ g2o/types/slam3d_new/isometry3d_mappings.cpp
@@ -24,7 +24,7 @@
 // NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 // SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-//#include "isometry3d_mappings.h"
+#include "isometry3d_mappings.h"
 
 namespace Slam3dNew {
   using namespace Eigen;
