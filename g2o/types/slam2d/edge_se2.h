d2e062a6cba72927ce18078f16cb89949e32194a
Rainer Kuemmerle <kuemmerl@informatik.uni-freiburg.de>
2012-10-02 15:27:47 +0200

    Calibration

--- g2o/types/slam2d/edge_se2.h
+++ g2o/types/slam2d/edge_se2.h
@@ -41,7 +41,7 @@ namespace g2o {
   {
     public:
       EIGEN_MAKE_ALIGNED_OPERATOR_NEW
-      EdgeSE2();
+        EdgeSE2();
 
       void computeError()
       {
@@ -54,22 +54,22 @@ namespace g2o {
       virtual bool write(std::ostream& os) const;
 
       virtual void setMeasurement(const SE2& m){
-  _measurement = m;
-  _inverseMeasurement = m.inverse();
+        _measurement = m;
+        _inverseMeasurement = m.inverse();
       }
 
       virtual bool setMeasurementData(const double* d){
-  _measurement=SE2(d[0], d[1], d[2]);
-  _inverseMeasurement = _measurement.inverse();
-  return true;
+        _measurement=SE2(d[0], d[1], d[2]);
+        _inverseMeasurement = _measurement.inverse();
+        return true;
       }
 
       virtual bool getMeasurementData(double* d) const {
-  Vector3d v=_measurement.toVector();
-  d[0] = v[0];
-  d[1] = v[1];
-  d[2] = v[2];
-  return true;
+        Vector3d v=_measurement.toVector();
+        d[0] = v[0];
+        d[1] = v[1];
+        d[2] = v[2];
+        return true;
       }
 
       virtual int measurementDimension() const {return 3;}
@@ -77,12 +77,12 @@ namespace g2o {
       virtual bool setMeasurementFromState() {
         const VertexSE2* v1 = static_cast<const VertexSE2*>(_vertices[0]);
         const VertexSE2* v2 = static_cast<const VertexSE2*>(_vertices[1]);
-  _measurement = v1->estimate().inverse()*v2->estimate();
-  _inverseMeasurement = _measurement.inverse();
-  return true;
+        _measurement = v1->estimate().inverse()*v2->estimate();
+        _inverseMeasurement = _measurement.inverse();
+        return true;
       }
 
-      
+
       virtual double initialEstimatePossible(const OptimizableGraph::VertexSet& , OptimizableGraph::Vertex* ) { return 1.;}
       virtual void initialEstimate(const OptimizableGraph::VertexSet& from, OptimizableGraph::Vertex* to);
 #ifndef NUMERIC_JACOBIAN_TWO_D_TYPES
