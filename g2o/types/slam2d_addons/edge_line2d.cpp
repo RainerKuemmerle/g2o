ef2d8533b95c95cfb5b1019035b1884dc5bfb4be
Giorgio Grisetti <grisetti@informatik.uni-freiburg.de>
2013-02-05 15:59:57 +0100

    added line2d-line2d edge by Martina Deturres

--- g2o/types/slam2d_addons/edge_line2d.cpp
+++ g2o/types/slam2d_addons/edge_line2d.cpp
@@ -24,20 +24,54 @@
 // NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 // SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-#ifndef G2O_TYPES_SLAM2D_SEGMENT_
-#define G2O_TYPES_SLAM2D_SEGMENT_
-
-#include "g2o/config.h"
-#include "g2o/types/slam2d/types_slam2d.h"
-
-// line slam ver 1
-#include "edge_se2_segment2d.h"
-#include "edge_se2_segment2d_line.h"
-#include "edge_se2_segment2d_pointLine.h"
-
-// line slam ver 2
-#include "edge_se2_line2d.h"
-#include "edge_line2d_pointxy.h"
 #include "edge_line2d.h"
 
+#ifdef G2O_HAVE_OPENGL
+#include "g2o/stuff/opengl_wrapper.h"
+#include "g2o/stuff/opengl_primitives.h"
 #endif
+
+  using namespace g2o;
+  using namespace Eigen;
+
+  EdgeLine2D::EdgeLine2D() :
+    BaseBinaryEdge<2, Vector2d, VertexLine2D, VertexLine2D>()
+  {
+    _information.setIdentity();
+    _error.setZero();
+  }
+
+  bool EdgeLine2D::read(std::istream& is)
+  {
+    Vector2d  v;
+    for (int i = 0; i < 2; ++i)
+      is >> v[i];
+    setMeasurement(v);
+    for (int i = 0; i < 2; ++i)
+      for (int j = i; j < 2; ++j) {
+        is >> information()(i, j);
+        if (i != j)
+          information()(j, i) = information()(i, j);
+      }
+    return true;
+  }
+
+  bool EdgeLine2D::write(std::ostream& os) const
+  {
+    for (int i = 0; i < 2; ++i)
+      os << _measurement[i] << " ";
+    for (int i = 0; i < 2; ++i)
+      for (int j = i; j < 2; ++j)
+        os << " " << information()(i, j);
+    return os.good();
+  }
+
+
+#ifndef NUMERIC_JACOBIAN_TWO_D_TYPES
+  void EdgeLine2D::linearizeOplus()
+  {
+    _jacobianOplusXi=-Matrix2d::Identity();
+    _jacobianOplusXj= Matrix2d::Identity();
+  }
+#endif
+
