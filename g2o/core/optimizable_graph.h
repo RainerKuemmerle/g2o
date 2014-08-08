747165feff87b3b1b337b502a660f43cc2d74438
Rainer Kuemmerle <kuemmerl@informatik.uni-freiburg.de>
2014-08-08 10:05:18 +0200

    fix some warnings found by clang

--- g2o/core/optimizable_graph.h
+++ g2o/core/optimizable_graph.h
@@ -523,7 +523,7 @@ namespace g2o {
      * overridden from HyperGraph, to mantain the bookkeeping of the caches/parameters and jacobian workspaces consistent upon a change in the veretx.
      * @return false if something goes wriong.
      */
-    virtual bool setEdgeVertex(Edge* e, int pos, Vertex* v);
+    virtual bool setEdgeVertex(HyperGraph::Edge* e, int pos, HyperGraph::Vertex* v);
 
     //! returns the chi2 of the current configuration
     double chi2() const;
