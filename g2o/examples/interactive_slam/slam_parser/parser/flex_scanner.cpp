433d2028dd693dd34aff1ae62448260ab660fc36
Rainer Kuemmerle <kuemmerl@informatik.uni-freiburg.de>
2014-08-09 19:02:13 +0200

    dropping register from the flex scanner

--- g2o/examples/interactive_slam/slam_parser/parser/flex_scanner.cpp
+++ g2o/examples/interactive_slam/slam_parser/parser/flex_scanner.cpp
@@ -613,9 +613,9 @@ static int yy_flex_strlen (yyconst char * );
  */
 YY_DECL
 {
-  register yy_state_type yy_current_state;
-  register char *yy_cp, *yy_bp;
-  register int yy_act;
+  yy_state_type yy_current_state;
+  char *yy_cp, *yy_bp;
+  int yy_act;
     
 #line 49 "scanner.l"
 
@@ -672,7 +672,7 @@ YY_DECL
 yy_match:
     do
       {
-      register YY_CHAR yy_c = yy_ec[YY_SC_TO_UI(*yy_cp)];
+      YY_CHAR yy_c = yy_ec[YY_SC_TO_UI(*yy_cp)];
       if ( yy_accept[yy_current_state] )
         {
         (yy_last_accepting_state) = yy_current_state;
@@ -1059,9 +1059,9 @@ void yyFlexLexer::LexerOutput( const char* buf, int size )
  */
 int yyFlexLexer::yy_get_next_buffer()
 {
-      register char *dest = YY_CURRENT_BUFFER_LVALUE->yy_ch_buf;
-  register char *source = (yytext_ptr);
-  register int number_to_move, i;
+      char *dest = YY_CURRENT_BUFFER_LVALUE->yy_ch_buf;
+  char *source = (yytext_ptr);
+  int number_to_move, i;
   int ret_val;
 
   if ( (yy_c_buf_p) > &YY_CURRENT_BUFFER_LVALUE->yy_ch_buf[(yy_n_chars) + 1] )
@@ -1193,14 +1193,14 @@ int yyFlexLexer::yy_get_next_buffer()
 
     yy_state_type yyFlexLexer::yy_get_previous_state()
 {
-  register yy_state_type yy_current_state;
-  register char *yy_cp;
+  yy_state_type yy_current_state;
+  char *yy_cp;
     
   yy_current_state = (yy_start);
 
   for ( yy_cp = (yytext_ptr) + YY_MORE_ADJ; yy_cp < (yy_c_buf_p); ++yy_cp )
     {
-    register YY_CHAR yy_c = (*yy_cp ? yy_ec[YY_SC_TO_UI(*yy_cp)] : 1);
+    YY_CHAR yy_c = (*yy_cp ? yy_ec[YY_SC_TO_UI(*yy_cp)] : 1);
     if ( yy_accept[yy_current_state] )
       {
       (yy_last_accepting_state) = yy_current_state;
@@ -1225,10 +1225,10 @@ int yyFlexLexer::yy_get_next_buffer()
  */
     yy_state_type yyFlexLexer::yy_try_NUL_trans( yy_state_type yy_current_state )
 {
-  register int yy_is_jam;
-      register char *yy_cp = (yy_c_buf_p);
+  int yy_is_jam;
+      char *yy_cp = (yy_c_buf_p);
 
-  register YY_CHAR yy_c = 1;
+  YY_CHAR yy_c = 1;
   if ( yy_accept[yy_current_state] )
     {
     (yy_last_accepting_state) = yy_current_state;
@@ -1246,9 +1246,9 @@ int yyFlexLexer::yy_get_next_buffer()
   return yy_is_jam ? 0 : yy_current_state;
 }
 
-    void yyFlexLexer::yyunput( int c, register char* yy_bp)
+    void yyFlexLexer::yyunput( int c, char* yy_bp)
 {
-  register char *yy_cp;
+  char *yy_cp;
     
     yy_cp = (yy_c_buf_p);
 
@@ -1258,10 +1258,10 @@ int yyFlexLexer::yy_get_next_buffer()
   if ( yy_cp < YY_CURRENT_BUFFER_LVALUE->yy_ch_buf + 2 )
     { /* need to shift things up to make room */
     /* +2 for EOB chars. */
-    register int number_to_move = (yy_n_chars) + 2;
-    register char *dest = &YY_CURRENT_BUFFER_LVALUE->yy_ch_buf[
+    int number_to_move = (yy_n_chars) + 2;
+    char *dest = &YY_CURRENT_BUFFER_LVALUE->yy_ch_buf[
           YY_CURRENT_BUFFER_LVALUE->yy_buf_size + 2];
-    register char *source =
+    char *source =
         &YY_CURRENT_BUFFER_LVALUE->yy_ch_buf[number_to_move];
 
     while ( source > YY_CURRENT_BUFFER_LVALUE->yy_ch_buf )
@@ -1687,7 +1687,7 @@ void yyFlexLexer::LexerError( yyconst char msg[] )
 #ifndef yytext_ptr
 static void yy_flex_strncpy (char* s1, yyconst char * s2, int n )
 {
-  register int i;
+  int i;
   for ( i = 0; i < n; ++i )
     s1[i] = s2[i];
 }
@@ -1696,7 +1696,7 @@ static void yy_flex_strncpy (char* s1, yyconst char * s2, int n )
 #ifdef YY_NEED_STRLEN
 static int yy_flex_strlen (yyconst char * s )
 {
-  register int n;
+  int n;
   for ( n = 0; s[n]; ++n )
     ;
 
