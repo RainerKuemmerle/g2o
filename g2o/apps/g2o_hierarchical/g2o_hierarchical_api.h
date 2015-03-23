ff7ae7e20eb7944e3cbd6f2eb08dff2943ab48bf
Hamdi Sahloul <hamdisahloul@hotmail.com>
2015-03-23 18:45:12 +0900

    Fixup of MSVC patch

--- g2o/apps/g2o_hierarchical/g2o_hierarchical_api.h
+++ g2o/apps/g2o_hierarchical/g2o_hierarchical_api.h
@@ -1,19 +1,3 @@
-// g2o - General Graph Optimization
-// Copyright (C) 2015 Hamdi Sahloul
-// 
-// g2o is free software: you can redistribute it and/or modify
-// it under the terms of the GNU Lesser General Public License as published
-// by the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-// 
-// g2o is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU Lesser General Public License for more details.
-// 
-// You should have received a copy of the GNU Lesser General Public License
-// along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
 #ifndef G2O_HIERARCHICAL_API_H
 #define G2O_HIERARCHICAL_API_H
 
@@ -21,7 +5,7 @@
 
 #ifdef _MSC_VER
 // We are using a Microsoft compiler:
-#ifdef G2O_LGPL_SHARED_LIBS
+#ifdef G2O_SHARED_LIBS
 #ifdef g2o_hierarchical_library_EXPORTS
 #define G2O_HIERARCHICAL_API __declspec(dllexport)
 #else
