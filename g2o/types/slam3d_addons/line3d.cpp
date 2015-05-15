1641ed4c296e7b14413817dfb0ed6279d887b117
Rainer Kuemmerle <rainer.kuemmerle@gmail.com>
2015-05-15 10:30:42 +0200

    adding license header

--- g2o/types/slam3d_addons/line3d.cpp
+++ g2o/types/slam3d_addons/line3d.cpp
@@ -1,3 +1,29 @@
+// g2o - General Graph Optimization
+// Copyright (C) 2011 R. Kuemmerle, G. Grisetti, W. Burgard
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are
+// met:
+//
+// * Redistributions of source code must retain the above copyright notice,
+//   this list of conditions and the following disclaimer.
+// * Redistributions in binary form must reproduce the above copyright
+//   notice, this list of conditions and the following disclaimer in the
+//   documentation and/or other materials provided with the distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+// IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+// TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+// PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+// HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+// TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
 #include "line3d.h"
 #include <iostream>
 
@@ -5,14 +31,14 @@ namespace g2o {
   using namespace std;
   using namespace Eigen;
 
-  inline void _skew(Matrix3D& S, const Vector3D& t){
+  static inline void _skew(Matrix3D& S, const Vector3D& t){
     S <<
       0,  -t.z(),   t.y(),
       t.z(),     0,     -t.x(),
       -t.y()     ,t.x(),   0;
   }
 
-  inline Matrix3D _skew(const Vector3D& t){
+  static inline Matrix3D _skew(const Vector3D& t){
     Matrix3D S;
     S <<
       0,  -t.z(),   t.y(),
