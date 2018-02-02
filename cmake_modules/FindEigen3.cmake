c681c25be94d354cbea7e669025bb810a65b9d85
Geoffrey Viola <gviola@autox.ai>
2018-02-01 19:44:32 -0800

    cpack support

--- cmake_modules/FindEigen3.cmake
+++ cmake_modules/FindEigen3.cmake
@@ -68,7 +68,7 @@ else (EIGEN3_INCLUDE_DIR)
 
   find_path(EIGEN3_INCLUDE_DIR NAMES signature_of_eigen3_matrix_library
       PATHS
-      ${CMAKE_INSTALL_PREFIX}/include
+      include
       ${EIGEN_ADDITIONAL_SEARCH_PATHS}
       ${KDE4_INCLUDE_DIR}
       PATH_SUFFIXES eigen3 eigen
