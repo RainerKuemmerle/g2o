6ca5a0cfb0b69a21357e8cccad74e65dd41b5230
Simon Julier <s.julier@ucl.ac.uk>
2018-08-13 17:03:25 +0100

    Copied back the old way of supporting eigen3. The version of linux used in travis seems to have a very old configuration of cmake which cannot cope with the recent eigen build system.

--- script/install-deps-linux.sh
+++ script/install-deps-linux.sh
@@ -5,13 +5,9 @@ set -ev
 sudo apt-get update -qq
 sudo apt-get install -qq qtdeclarative5-dev qt5-qmake libqglviewer-dev libsuitesparse-dev
 
-# download eigen3, unpack and install it. The installation is required to install the cmake files
+# download eigen3 and unpack it
 cd /tmp
 wget -O eigen3.zip http://bitbucket.org/eigen/eigen/get/3.3.4.zip
 unzip -q eigen3.zip
 ls -l eigen*
-mkdir eigen-eigen-5a0156e40feb-build
-cd eigen-eigen-5a0156e40feb-build
-cmake ../eigen-eigen-5a0156e40feb -DBUILD_TESTING=OFF -DCMAKE_INSTALL_PREFIX=/usr/include
-sudo make install
-cd ..
+sudo mv /tmp/eigen-eigen-5a0156e40feb /usr/include/eigen3
