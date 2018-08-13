42cfd32558a465a2df31b6fbf109545fe3ec9456
Simon Julier <s.julier@ucl.ac.uk>
2018-08-13 16:49:06 +0100

    Download, build and install the eigen3 dependency. Building is needed to create the configuration files.

--- script/install-deps-linux.sh
+++ script/install-deps-linux.sh
@@ -5,9 +5,13 @@ set -ev
 sudo apt-get update -qq
 sudo apt-get install -qq qtdeclarative5-dev qt5-qmake libqglviewer-dev libsuitesparse-dev
 
-# download eigen3 and unpack it
+# download eigen3, unpack and install it. The installation is required to install the cmake files
 cd /tmp
 wget -O eigen3.zip http://bitbucket.org/eigen/eigen/get/3.3.4.zip
 unzip -q eigen3.zip
 ls -l eigen*
-sudo mv /tmp/eigen-eigen-5a0156e40feb /usr/include/eigen3
+mkdir eigen-eigen-5a0156e40feb-build
+pushd eigen-eigen-5a0156e40feb-build > /dev/null
+cmake ../eigen-eigen-5a0156e40feb -DBUILD_TESTING=OFF -DCMAKE_INSTALL_PREFIX=/usr/include
+sudo make install
+popd > /dev/null
