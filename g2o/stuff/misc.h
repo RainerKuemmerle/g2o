905fd44e05c2612710ff516529426ae8ef5ff579
O B thuis <you@example.com>
2018-10-22 17:52:48 +0200

    moved constexpr sqrt to stuff/misc.h

--- g2o/stuff/misc.h
+++ g2o/stuff/misc.h
@@ -218,6 +218,31 @@ struct ForceLinker
     ForceLinker(ForceLinkFunction function) { (function)(); }
 };
 
+namespace internal {
+  template <typename T>
+  constexpr T sqrt_helper(T x, T lo, T hi)
+  {
+    if (lo == hi)
+      return lo;
+
+    const T mid = (lo + hi + 1) / 2;
+
+    if (x / mid < mid)
+      return sqrt_helper<T>(x, lo, mid - 1);
+    else
+      return sqrt_helper(x, mid, hi);
+  }
+}
+
+/**
+ * a constexpr integer sqrt function
+ * adapted from https://stackoverflow.com/a/27709195
+ */
+template <typename T>
+constexpr T ct_sqrt(T x)
+{
+  return internal::sqrt_helper<T>(x, 0, x / 2 + 1);
+}
 
 } // end namespace
 
