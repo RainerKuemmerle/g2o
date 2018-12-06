48eca4bb95541d58715be6693ce3f6e720bf656e
O B thuis <you@example.com>
2018-12-06 23:03:14 +0100

    removed #pragma once

--- g2o/core/base_fixed_sized_edge.hpp
+++ g2o/core/base_fixed_sized_edge.hpp
@@ -27,8 +27,6 @@
 // NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 // SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-#pragma once
-
 namespace internal {
 
 struct QuadraticFormLock {
