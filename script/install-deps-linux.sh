e2f5024dd4bf8eab96e897c6f386fa07dbf28f0e
Rainer Kuemmerle <rainer.kuemmerle@gmail.com>
2020-11-07 17:45:29 +0100

    Travis update

--- script/install-deps-linux.sh
+++ script/install-deps-linux.sh
@@ -3,7 +3,7 @@
 set -ev
 
 # source for new gcc
-sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
+#sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
 
 sudo apt-get update -qq
 
@@ -11,14 +11,14 @@ sudo apt-get update -qq
 sudo apt-get install -qq qtdeclarative5-dev qt5-qmake libqglviewer-dev-qt5 libsuitesparse-dev
 
 # install new gcc
-if [ "$CC" = "gcc" ]; then
-  apt-cache search -n "^gcc-[0-9]+$"
-  sudo apt-get install -qq gcc-8 g++-8
-fi
+#if [ "$CC" = "gcc" ]; then
+  #apt-cache search -n "^gcc-[0-9]+$"
+  #sudo apt-get install -qq gcc-8 g++-8
+#fi
 
 # download eigen3 and unpack it
 cd /tmp
-wget -O eigen3.zip https://gitlab.com/libeigen/eigen/-/archive/3.3.7/eigen-3.3.7.zip
+wget -O eigen3.zip https://gitlab.com/libeigen/eigen/-/archive/3.3.8/eigen-3.3.8.zip
 unzip -q eigen3.zip
 ls -l eigen*
-sudo mv /tmp/eigen-3.3.7 /usr/include/eigen3
+sudo mv /tmp/eigen-3.3.8 /usr/include/eigen3
