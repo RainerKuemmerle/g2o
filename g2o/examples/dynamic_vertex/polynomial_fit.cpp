22b06209ede1da40840beb34f6ee8cbfe0a81658
Simon Julier <s.julier@ucl.ac.uk>
2020-11-09 11:25:01 +0000

    Use changeEstimateDimensionImpl. Tidied up.

--- g2o/examples/dynamic_vertex/polynomial_fit.cpp
+++ g2o/examples/dynamic_vertex/polynomial_fit.cpp
@@ -1,7 +1,7 @@
 // This example illustrates how to use a dynamic vertex in a graph.
 
 // The goal is to fit a polynomial y=p(x) to a set of data. The degree of the
-// polynomial is user-defined.
+// polynomial is user-defined. The amount of samples is user defined as well
 
 #include <unsupported/Eigen/Polynomials>
 
@@ -24,9 +24,8 @@ class PolynomialCoefficientVertex : public g2o::BaseVertex<Eigen::Dynamic, Eigen
 public:
   EIGEN_MAKE_ALIGNED_OPERATOR_NEW;
 
-  // Create the vertex and assign the starting dimension
-  PolynomialCoefficientVertex(int startingDimension = 0) {
-    setEstimateDimension(startingDimension);
+  // Create the vertex
+  PolynomialCoefficientVertex() {
   }
 
   // Read the vertex
@@ -39,8 +38,8 @@ public:
       return false;
     }
 
-    // Resize; call the resize method because this ensures that the Jacobian
-    // workspace is properly updated
+    // Set the dimension; we call the method here to ensure stuff like
+    // cache and the workspace is setup
     setEstimateDimension(dimension);
 
     // Read the state
@@ -62,29 +61,26 @@ public:
   // Direct linear add
   virtual void oplusImpl(const double* update) {
     Eigen::VectorXd::ConstMapType v(update, _dimension);
-    //std::cout << v << std::endl;
     _estimate += v;
   }
 
-  // Resize the vertex state. This uses Eigen's conservative resize.
-  // Note we do not zero out the vector to keep what coefficients can be
-  // kept which changing size.
-  virtual bool setEstimateDimensionImpl(int newDimension)
+  // Resize the vertex state. In this case, we want to preserve as much of the
+  // state as we can. Therefore, we use conservativeResize and pad with zeros
+  // at the end if the state dimension has increased.
+  virtual bool changeEstimateDimensionImpl(int newDimension)
   {
     int oldDimension = estimateDimension();
 
     // Handle the special case this is the first time
-    //if (_dimension == Eigen::Dynamic) {
-      std::cout << "*" << std::endl;
+    if (oldDimension == Eigen::Dynamic) {
       _estimate.resize(newDimension);
       _estimate.setZero();
       return true;
-      //}
+    }
 
     _estimate.conservativeResize(newDimension);
 
-    // If the state has expanded, set the new values to zero
-    std::cout << oldDimension << ":" << newDimension << std::endl;
+    // If the state has expanded, pad with zeros
     if (oldDimension < newDimension)
       _estimate.tail(newDimension-oldDimension).setZero();
 
@@ -94,7 +90,7 @@ public:
 
 // This edge provides an observation of the polynomial at a single value.
 // The assumed model is z = p(x) + w,
-// where w is the additive noise with covariance equal to Omega.
+// where w is the additive noise with covariance equal to inv(Omega).
 
 // Note that x is not a measurement so it has to be stored separately.
 
@@ -136,7 +132,7 @@ private:
 
 int main(int argc, const char* argv[]) {
 
-  // Set ground truth dimensions
+  // Number of dimensions of the polynomial; the default is 4
   int polynomialDimension = 4;
   if (argc > 1) {
     polynomialDimension = atoi(argv[1]);
@@ -150,7 +146,7 @@ int main(int argc, const char* argv[]) {
 
   std::cout << "Ground truth vector=" << p.transpose() << std::endl;
 
-  // Set number of observations
+  // The number of observations in the polynomial; the defautl is 6
   int obs = 6;
   if (argc > 2) {
     obs = atoi(argv[2]);
@@ -183,7 +179,6 @@ int main(int argc, const char* argv[]) {
   optimizer->setVerbose(true);
   optimizer->setAlgorithm(optimisationAlgorithm);
 
-
   // Create the vertex; note its dimension is currently is undefined
   PolynomialCoefficientVertex* pv = new PolynomialCoefficientVertex();
   pv->setId(0);
@@ -202,23 +197,21 @@ int main(int argc, const char* argv[]) {
       optimizer->addEdge(pe);
     }
 
-  // Iterate over different vertex sizes from 1 to the number of dimensions
+  // Now run the same optimization problem for different choices of
+  // dimension of the polynomial vertex. This shows how we can
+  // dynamically change the vertex dimensions in an alreacy
+  // constructed graph. Note that you must call initializeOptimization
+  // before you can optimize after a state dimension has changed.
   for (int testDimension = 1; testDimension <= polynomialDimension; ++testDimension) {
     pv->setEstimateDimension(testDimension);
     optimizer->initializeOptimization();
     optimizer->optimize(10);
     std::cout << "Computed parameters = " << pv->estimate().transpose() << std::endl;
   }
-  
-  /*
-  Eigen::VectorXd x(5);
-  x[0] = -2;
-  x[1] = -1;
-  x[2] = 0;
-  x[3] = 1;
-  x[4] = 2;
-
-  for (int i = 0; i < 5; ++i) {
-    std::cout << x[i] << ":" << Eigen::poly_eval(p, x[i]) << std::endl;
-    }*/
+  for (int testDimension = polynomialDimension - 1; testDimension >= 1; --testDimension) {
+    pv->setEstimateDimension(testDimension);
+    optimizer->initializeOptimization();
+    optimizer->optimize(10);
+    std::cout << "Computed parameters = " << pv->estimate().transpose() << std::endl;
+  }
 }
