9383efbdd1ee966aef216c834e5b6e794a600c29
Rainer Kuemmerle <rainer.kuemmerle@gmail.com>
2020-11-14 18:02:43 +0100

    Resolving warning related to Qt and QGLViewer

--- g2o/apps/g2o_viewer/gui_hyper_graph_action.cpp
+++ g2o/apps/g2o_viewer/gui_hyper_graph_action.cpp
@@ -2,17 +2,17 @@
 // Copyright (C) 2011 R. Kuemmerle, G. Grisetti, W. Burgard
 //
 // This file is part of g2o.
-// 
+//
 // g2o is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
-// 
+//
 // g2o is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
-// 
+//
 // You should have received a copy of the GNU General Public License
 // along with g2o.  If not, see <http://www.gnu.org/licenses/>.
 
@@ -21,6 +21,7 @@
 #include "g2o_qglviewer.h"
 
 #include <QApplication>
+#include <QtGlobal>
 
 namespace g2o {
 
@@ -46,7 +47,11 @@ HyperGraphAction* GuiHyperGraphAction::operator()(const HyperGraph* graph, Param
       if (p) {
         viewer->setSnapshotFormat(QString("PNG"));
         viewer->setSnapshotQuality(-1);
+#if QT_VERSION < QT_VERSION_CHECK(5, 5, 0)
         viewer->saveSnapshot(QString().sprintf("g2o%.6d.png", p->iteration), true);
+#else
+        viewer->saveSnapshot(QString().asprintf("g2o%.6d.png", p->iteration), true);
+#endif
       }
     }
 
