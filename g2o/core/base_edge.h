b92558c15114d83759b7d9edcb1ed8ec30933e7d
Rainer Kuemmerle <rainer.kuemmerle@gmail.com>
2020-11-14 16:58:53 +0100

    Avoid template specialization of BaseEdge
    * use a traits template for the type definition to avoid duplication of
      code in the specialization of BaseEdge.

--- g2o/core/base_edge.h
+++ g2o/core/base_edge.h
@@ -57,6 +57,20 @@ namespace g2o {
     };
   #endif
 
+    template <int D>
+    struct BaseEdgeTraits {
+      static constexpr int Dimension = D;
+      typedef Eigen::Matrix<number_t, D, 1, Eigen::ColMajor> ErrorVector;
+      typedef Eigen::Matrix<number_t, D, D, Eigen::ColMajor> InformationType;
+    };
+
+    template <>
+    struct BaseEdgeTraits<-1> {
+      static constexpr int Dimension = -1;
+      typedef Eigen::Matrix<number_t, Eigen::Dynamic, 1, Eigen::ColMajor> ErrorVector;
+      typedef Eigen::Matrix<number_t, Eigen::Dynamic, Eigen::Dynamic, Eigen::ColMajor> InformationType;
+    };
+
   } // internal namespace
 
   template <int D, typename E>
@@ -64,10 +78,10 @@ namespace g2o {
   {
     public:
 
-      static const int Dimension = D;
+      static constexpr int Dimension = internal::BaseEdgeTraits<D>::Dimension;
       typedef E Measurement;
-      typedef Eigen::Matrix<number_t, D, 1, Eigen::ColMajor> ErrorVector;
-      typedef Eigen::Matrix<number_t, D, D, Eigen::ColMajor> InformationType;
+      typedef typename internal::BaseEdgeTraits<D>::ErrorVector ErrorVector;
+      typedef typename internal::BaseEdgeTraits<D>::InformationType InformationType;
 
       BaseEdge() : OptimizableGraph::Edge()
       {
@@ -154,74 +168,6 @@ namespace g2o {
     public:
       EIGEN_MAKE_ALIGNED_OPERATOR_NEW
   };
-
-
-  template<typename E>
-  class BaseEdge<-1,E> : public OptimizableGraph::Edge
-  {
-    public:
-
-      static const int Dimension = -1;
-      typedef E Measurement;
-      typedef Eigen::Matrix<number_t, Eigen::Dynamic, 1, Eigen::ColMajor> ErrorVector;
-      typedef Eigen::Matrix<number_t, Eigen::Dynamic, Eigen::Dynamic, Eigen::ColMajor> InformationType;
-
-      BaseEdge() : OptimizableGraph::Edge(){
-
-      }
-
-      virtual ~BaseEdge() {}
-
-      virtual number_t chi2() const
-      {
-        return _error.dot(information()*_error);
-      }
-
-      virtual const number_t* errorData() const { return _error.data();}
-      virtual number_t* errorData() { return _error.data();}
-      const ErrorVector& error() const { return _error;}
-      ErrorVector& error() { return _error;}
-
-      //! information matrix of the constraint
-      const InformationType& information() const { return _information;}
-      InformationType& information() { return _information;}
-      void setInformation(const InformationType& information) { _information = information;}
-
-      virtual const number_t* informationData() const { return _information.data();}
-      virtual number_t* informationData() { return _information.data();}
-
-      //! accessor functions for the measurement represented by the edge
-      const Measurement& measurement() const { return _measurement;}
-      virtual void setMeasurement(const Measurement& m) { _measurement = m;}
-
-      virtual int rank() const {return _dimension;}
-
-      virtual void initialEstimate(const OptimizableGraph::VertexSet&, OptimizableGraph::Vertex*)
-      {
-        std::cerr << "inititialEstimate() is not implemented, please give implementation in your derived class" << std::endl;
-      }
-
-    protected:
-
-      Measurement _measurement;
-      InformationType _information;
-      ErrorVector _error;
-
-      /**
-       * calculate the robust information matrix by updating the information matrix of the error
-       */
-      InformationType robustInformation(const Vector3& rho)
-      {
-        InformationType result = rho[1] * _information;
-        //ErrorVector weightedErrror = _information * _error;
-        //result.noalias() += 2 * rho[2] * (weightedErrror * weightedErrror.transpose());
-        return result;
-      }
-
-    public:
-      EIGEN_MAKE_ALIGNED_OPERATOR_NEW
-  };
-
 
 } // end namespace g2o
 
