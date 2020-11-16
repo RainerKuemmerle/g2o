2fb97e58c87d2898d4ca5a0fead8a918be7fa989
Simon Julier <s.julier@ucl.ac.uk>
2020-11-16 09:17:49 +0000

    Use the g2o dynamic_aligned_buffer.

--- g2o/benchmarks/jacobian_timing_tests.cpp
+++ g2o/benchmarks/jacobian_timing_tests.cpp
@@ -1,82 +1,9 @@
 #include <benchmark/benchmark.h>
-#include <Eigen/Core>
+
+#include "g2o/core/dynamic_aligned_buffer.hpp"
 
 // Test several different ways of evaluating Jacobians to see the impact of different ways of implementing stuff.
 
-// 16 byte aligned allocation functions
-template<typename Type>
-Type* allocate_aligned(size_t n)
-{
-  return (Type*)Eigen::internal::aligned_malloc(n * sizeof(Type));
-}
-
-template<typename Type>
-Type* reallocate_aligned(Type* ptr, size_t newSize, size_t oldSize)
-{
-  return (Type*)Eigen::internal::aligned_realloc(ptr, newSize * sizeof(Type), oldSize * sizeof(Type));
-}
-
-template<typename Type>
-void free_aligned(Type* block)
-{
-  Eigen::internal::aligned_free(block);
-}
-
-template<typename Type>
-struct dynamic_aligned_buffer
-{
-  dynamic_aligned_buffer(size_t size)
-    : m_size{ 0 }, m_ptr{ nullptr }
-  {
-    allocate(size);
-  }
-
-  ~dynamic_aligned_buffer()
-  {
-    free();
-  }
-
-  Type* request(size_t n)
-  {
-    if (n <= m_size)
-      return m_ptr;
-
-    m_ptr = reallocate_aligned<Type>(m_ptr, n, m_size);
-    m_size = m_ptr ? n : 0;
-
-    return m_ptr;
-  }
-
-private:
-  void allocate(size_t size)
-  {
-    m_ptr = allocate_aligned<Type>(size);
-    if (m_ptr != nullptr)
-      m_size = size;
-  }
-
-  void free()
-  {
-    if (m_ptr != nullptr)
-      {
-        free_aligned<Type>(m_ptr);
-        m_size = 0;
-        m_ptr = nullptr;
-      }
-  }
-
-  std::size_t m_size;
-  Type* m_ptr;
-};
-
-template<typename T>
-struct aligned_deleter
-{
-  void operator()(T* block)
-  {
-    free_aligned(block);
-  }
-};
 
 template<typename number_t, int D>
 void BM_FixedArray(benchmark::State& state)
@@ -238,7 +165,7 @@ void BM_DynamicAlignedBuffer(benchmark::State& state)
   while (state.KeepRunning())
     {
       const int d = state.range(0);
-      static dynamic_aligned_buffer<number_t> buffer{ size_t(d)};
+      static g2o::dynamic_aligned_buffer<number_t> buffer{ size_t(d)};
       number_t* add_vi = buffer.request(d);
       std::fill(add_vi, add_vi + d, number_t(0.0));
       for (int i = 0; i < d; ++i)
@@ -258,7 +185,7 @@ void BM_DynamicAlignedBufferPointer(benchmark::State& state)
   while (state.KeepRunning())
     {
       const int d = state.range(0);
-      static dynamic_aligned_buffer<number_t> buffer{ size_t(d) };
+      static g2o::dynamic_aligned_buffer<number_t> buffer{ size_t(d) };
       number_t* add_vi = buffer.request(d);
       std::fill(add_vi, add_vi + d, number_t(0.0));
       number_t* v= add_vi;
@@ -293,7 +220,7 @@ void BM_StaticDynamicDynamicAlignedBufferHybrid(benchmark::State& state)
         }
       else
         {
-          static dynamic_aligned_buffer<number_t> buffer{ size_t(d) };
+          static g2o::dynamic_aligned_buffer<number_t> buffer{ size_t(d) };
           number_t* add_vi = buffer.request(d);
           std::fill(add_vi, add_vi + d, number_t(0.0));
           for (int i = 0; i < d; ++i)
