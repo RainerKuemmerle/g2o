375710c790b1c66beb55d5693aa14e60035ecbd2
Rainer Kuemmerle <rainer.kuemmerle@gmail.com>
2020-12-14 19:57:53 +0100

    Extending test suite for linear solver

---
+++ unit_test/solver/linear_solver_test.cpp
@@ -0,0 +1,106 @@
+// g2o - General Graph Optimization
+// Copyright (C) 2011 R. Kuemmerle, G. Grisetti, W. Burgard
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are
+// met:
+//
+// * Redistributions of source code must retain the above copyright notice,
+//   this list of conditions and the following disclaimer.
+// * Redistributions in binary form must reproduce the above copyright
+//   notice, this list of conditions and the following disclaimer in the
+//   documentation and/or other materials provided with the distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+// IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+// TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+// PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+// HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+// TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+// clang-format off
+#include "g2o/config.h"
+// clang-format on
+
+#include "g2o/solvers/eigen/linear_solver_eigen.h"
+#ifdef G2O_HAVE_CSPARSE
+#include "g2o/solvers/csparse/linear_solver_csparse.h"
+#endif
+#ifdef G2O_HAVE_CHOLMOD
+#include "g2o/solvers/cholmod/linear_solver_cholmod.h"
+#endif
+#include "gtest/gtest.h"
+#include "sparse_system_helper.h"
+
+struct BlockOrdering {
+  static constexpr bool blockOrdering = true;
+};
+
+struct NoBlockOrdering {
+  static constexpr bool blockOrdering = false;
+};
+
+/**
+ * Type parameterized class for a fixture to setup a linear solver along with some data of a linear
+ * system to be solved.
+ */
+template <typename T>
+class LS : public testing::Test {
+ public:
+  using LinearSolverType = typename T::first_type;
+  using OrderingType = typename T::second_type;
+
+  LS() : linearsolver(g2o::make_unique<LinearSolverType>()) {}
+
+ protected:
+  std::unique_ptr<LinearSolverType> linearsolver;
+  g2o::SparseBlockMatrixX sparse_matrix = g2o::internal::createTestMatrix();
+  g2o::MatrixX matrix_inverse = g2o::internal::createTestMatrixInverse();
+  g2o::VectorX x_vector = g2o::internal::createTestVectorX();
+  g2o::VectorX b_vector = g2o::internal::createTestVectorB();
+};
+TYPED_TEST_SUITE_P(LS);
+
+TYPED_TEST_P(LS, Solve) {
+  this->linearsolver->setBlockOrdering(TypeParam::second_type::blockOrdering);
+
+  g2o::VectorX solver_solution;
+  solver_solution.setZero(this->b_vector.size());
+  this->linearsolver->solve(this->sparse_matrix, solver_solution.data(), this->b_vector.data());
+
+  ASSERT_TRUE(solver_solution.isApprox(this->x_vector, 1e-6));
+}
+
+TYPED_TEST_P(LS, SolvePattern) {
+  this->linearsolver->setBlockOrdering(TypeParam::second_type::blockOrdering);
+
+  g2o::SparseBlockMatrixX spinv;
+  std::vector<std::pair<int, int> > blockIndices;
+  for (int i = 0; i < static_cast<int>(this->sparse_matrix.rowBlockIndices().size()); ++i)
+    blockIndices.emplace_back(std::make_pair(i, i));
+
+  this->linearsolver->solvePattern(spinv, blockIndices, this->sparse_matrix);
+
+  // TODO compare solved pattern with the expected inverse
+}
+
+REGISTER_TYPED_TEST_SUITE_P(LS, Solve, SolvePattern);
+
+using LinearSolverTypes = ::testing::Types<
+#ifdef G2O_HAVE_CSPARSE
+    std::pair<g2o::LinearSolverCSparse<g2o::MatrixX>, NoBlockOrdering>,
+    std::pair<g2o::LinearSolverCSparse<g2o::MatrixX>, BlockOrdering>,
+#endif
+#ifdef G2O_HAVE_CHOLMOD
+    std::pair<g2o::LinearSolverCholmod<g2o::MatrixX>, NoBlockOrdering>,
+    std::pair<g2o::LinearSolverCholmod<g2o::MatrixX>, BlockOrdering>,
+#endif
+    std::pair<g2o::LinearSolverEigen<g2o::MatrixX>, NoBlockOrdering>,
+    std::pair<g2o::LinearSolverEigen<g2o::MatrixX>, BlockOrdering> >;
+INSTANTIATE_TYPED_TEST_SUITE_P(LinearSolver, LS, LinearSolverTypes);
