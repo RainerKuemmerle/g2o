bd17fe7a469e1ed7617447dd01c3ccb24eabce8e
Simon Julier <s.julier@ucl.ac.uk>
2021-03-03 12:37:43 +0000

    Fixed vcpkg package calls. Fixed dependencies. Added error checking if the vcpkg directory is incorrect.

--- script/install-deps-windows.bat
+++ script/install-deps-windows.bat
@@ -10,18 +10,24 @@ set "VcPkgTriplet=x64-windows"
 if defined VCPKG_ROOT_DIR if /i not "%VCPKG_ROOT_DIR%"=="" set "VcPkgDir=%VCPKG_ROOT_DIR%"
 if defined VCPKG_DEFAULT_TRIPLET if /i not "%VCPKG_DEFAULT_TRIPLET%"=="" set "VcPkgTriplet=%VCPKG_DEFAULT_TRIPLET%"
     
+if not exist %VcPkgDir%\ (
+echo Cannot find vcpkg directory %VcPkgDir%
+echo Set VCPKG_ROOT_DIR to override
+exit /b
+)
+
 pushd %VcPkgDir%
 
 rem ==============================
 rem Upgrade and Install packages.
 rem ==============================
-set "VcPkgLibs=eigen3 suitesparse clapack openblas ceres"
+set "VcPkgLibs=eigen3 lapack-reference openblas suitesparse ceres"
 
 echo vcpkg found at %VcPkgDir%...
 echo installing %VcPkgLibs% for triplet %VcPkgTriplet%...
 
-call %VcPkgDir% upgrade %VcPkgLibs% --no-dry-run --triplet %VcPkgTriplet%
-call %VcPkgDir% install %VcPkgLibs% --triplet %VcPkgTriplet%
+call %VcPkgDir%\vcpkg.exe upgrade %VcPkgLibs% --no-dry-run --triplet %VcPkgTriplet%
+call %VcPkgDir%\vcpkg.exe install %VcPkgLibs% --triplet %VcPkgTriplet%
 
 popd
 
