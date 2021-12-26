64cb355d0c2ab96c943c55cf7c85e1ef761942ae
Rainer Kuemmerle <rainer.kuemmerle@gmail.com>
2021-12-26 15:18:03 +0100

    Clean BA demos

--- g2o/examples/ba/ba_demo.cpp
+++ g2o/examples/ba/ba_demo.cpp
@@ -24,8 +24,7 @@
 // NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 // SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-#include <stdint.h>
-
+#include <cstdint>
 #include <iostream>
 #include <unordered_set>
 
@@ -44,8 +43,8 @@ G2O_USE_OPTIMIZATION_LIBRARY(eigen);
 
 G2O_USE_OPTIMIZATION_LIBRARY(dense);
 
-using namespace Eigen;
-using namespace std;
+using std::cout;
+using std::endl;
 
 class Sample {
  public:
@@ -103,7 +102,7 @@ int main(int argc, const char* argv[]) {
 
   g2o::SparseOptimizer optimizer;
   optimizer.setVerbose(false);
-  string solverName = "lm_fix6_3";
+  std::string solverName = "lm_fix6_3";
   if (DENSE) {
     solverName = "lm_dense6_3";
   } else {
@@ -118,17 +117,17 @@ int main(int argc, const char* argv[]) {
   optimizer.setAlgorithm(
       g2o::OptimizationAlgorithmFactory::instance()->construct(solverName, solverProperty));
 
-  vector<Vector3d> true_points;
+  std::vector<g2o::Vector3> true_points;
   for (size_t i = 0; i < 500; ++i) {
-    true_points.push_back(Vector3d((g2o::Sampler::uniformRand(0., 1.) - 0.5) * 3,
+    true_points.emplace_back((g2o::Sampler::uniformRand(0., 1.) - 0.5) * 3,
                                    g2o::Sampler::uniformRand(0., 1.) - 0.5,
-                                   g2o::Sampler::uniformRand(0., 1.) + 3));
+                                   g2o::Sampler::uniformRand(0., 1.) + 3);
   }
 
   double focal_length = 1000.;
-  Vector2d principal_point(320., 240.);
+  g2o::Vector2 principal_point(320., 240.);
 
-  vector<g2o::SE3Quat, aligned_allocator<g2o::SE3Quat> > true_poses;
+  std::vector<g2o::SE3Quat, Eigen::aligned_allocator<g2o::SE3Quat> > true_poses;
   auto cam_params = std::make_shared<g2o::CameraParameters>(focal_length, principal_point, 0.);
   cam_params->setId(0);
 
@@ -138,7 +137,7 @@ int main(int argc, const char* argv[]) {
 
   int vertex_id = 0;
   for (size_t i = 0; i < 15; ++i) {
-    Vector3d trans(i * 0.04 - 1., 0, 0);
+    g2o::Vector3 trans(i * 0.04 - 1., 0, 0);
 
     Eigen::Quaterniond q;
     q.setIdentity();
@@ -158,19 +157,19 @@ int main(int argc, const char* argv[]) {
   double sum_diff2 = 0;
 
   cout << endl;
-  unordered_map<int, int> pointid_2_trueid;
-  unordered_set<int> inliers;
+  std::unordered_map<int, int> pointid_2_trueid;
+  std::unordered_set<int> inliers;
 
   for (size_t i = 0; i < true_points.size(); ++i) {
     auto v_p = std::make_shared<g2o::VertexPointXYZ>();
     v_p->setId(point_id);
     v_p->setMarginalized(true);
-    v_p->setEstimate(true_points.at(i) + Vector3d(g2o::Sampler::gaussRand(0., 1),
+    v_p->setEstimate(true_points.at(i) + g2o::Vector3(g2o::Sampler::gaussRand(0., 1),
                                                   g2o::Sampler::gaussRand(0., 1),
                                                   g2o::Sampler::gaussRand(0., 1)));
     int num_obs = 0;
-    for (size_t j = 0; j < true_poses.size(); ++j) {
-      Vector2d z = cam_params->cam_map(true_poses.at(j).map(true_points.at(i)));
+    for (auto & true_pose : true_poses) {
+      g2o::Vector2 z = cam_params->cam_map(true_pose.map(true_points.at(i)));
       if (z[0] >= 0 && z[1] >= 0 && z[0] < 640 && z[1] < 480) {
         ++num_obs;
       }
@@ -179,21 +178,21 @@ int main(int argc, const char* argv[]) {
       optimizer.addVertex(v_p);
       bool inlier = true;
       for (size_t j = 0; j < true_poses.size(); ++j) {
-        Vector2d z = cam_params->cam_map(true_poses.at(j).map(true_points.at(i)));
+        g2o::Vector2 z = cam_params->cam_map(true_poses.at(j).map(true_points.at(i)));
 
         if (z[0] >= 0 && z[1] >= 0 && z[0] < 640 && z[1] < 480) {
           double sam = g2o::Sampler::uniformRand(0., 1.);
           if (sam < OUTLIER_RATIO) {
-            z = Vector2d(Sample::uniform(0, 640), Sample::uniform(0, 480));
+            z = g2o::Vector2(Sample::uniform(0, 640), Sample::uniform(0, 480));
             inlier = false;
           }
-          z += Vector2d(g2o::Sampler::gaussRand(0., PIXEL_NOISE),
+          z += g2o::Vector2(g2o::Sampler::gaussRand(0., PIXEL_NOISE),
                         g2o::Sampler::gaussRand(0., PIXEL_NOISE));
           auto e = std::make_shared<g2o::EdgeProjectXYZ2UV>();
           e->setVertex(0, v_p);
           e->setVertex(1, optimizer.vertices().find(j)->second);
           e->setMeasurement(z);
-          e->information() = Matrix2d::Identity();
+          e->information() = g2o::Matrix2::Identity();
           if (ROBUST_KERNEL) {
             e->setRobustKernel(std::make_shared<g2o::RobustKernelHuber>());
           }
@@ -204,11 +203,11 @@ int main(int argc, const char* argv[]) {
 
       if (inlier) {
         inliers.insert(point_id);
-        Vector3d diff = v_p->estimate() - true_points[i];
+        g2o::Vector3 diff = v_p->estimate() - true_points[i];
 
         sum_diff2 += diff.dot(diff);
       }
-      pointid_2_trueid.insert(make_pair(point_id, i));
+      pointid_2_trueid.insert(std::make_pair(point_id, i));
       ++point_id;
       ++point_num;
     }
@@ -220,9 +219,8 @@ int main(int argc, const char* argv[]) {
     g2o::StructureOnlySolver<3> structure_only_ba;
     cout << "Performing structure-only BA:" << endl;
     g2o::OptimizableGraph::VertexContainer points;
-    for (g2o::OptimizableGraph::VertexIDMap::const_iterator it = optimizer.vertices().begin();
-         it != optimizer.vertices().end(); ++it) {
-      auto v = std::static_pointer_cast<g2o::OptimizableGraph::Vertex>(it->second);
+    for (const auto & it : optimizer.vertices()) {
+      auto v = std::static_pointer_cast<g2o::OptimizableGraph::Vertex>(it.second);
       if (v->dimension() == 3) points.push_back(v);
     }
     structure_only_ba.calc(points, 10);
@@ -236,20 +234,19 @@ int main(int argc, const char* argv[]) {
        << endl;
   point_num = 0;
   sum_diff2 = 0;
-  for (unordered_map<int, int>::iterator it = pointid_2_trueid.begin();
-       it != pointid_2_trueid.end(); ++it) {
-    g2o::HyperGraph::VertexIDMap::iterator v_it = optimizer.vertices().find(it->first);
+  for (auto & it : pointid_2_trueid) {
+    auto v_it = optimizer.vertices().find(it.first);
     if (v_it == optimizer.vertices().end()) {
-      cerr << "Vertex " << it->first << " not in graph!" << endl;
+      std::cerr << "Vertex " << it.first << " not in graph!" << endl;
       exit(-1);
     }
-    g2o::VertexPointXYZ* v_p = dynamic_cast<g2o::VertexPointXYZ*>(v_it->second.get());
-    if (v_p == 0) {
-      cerr << "Vertex " << it->first << "is not a PointXYZ!" << endl;
+    auto* v_p = dynamic_cast<g2o::VertexPointXYZ*>(v_it->second.get());
+    if (v_p == nullptr) {
+      std::cerr << "Vertex " << it.first << "is not a PointXYZ!" << endl;
       exit(-1);
     }
-    Vector3d diff = v_p->estimate() - true_points[it->second];
-    if (inliers.find(it->first) == inliers.end()) continue;
+    g2o::Vector3 diff = v_p->estimate() - true_points[it.second];
+    if (inliers.find(it.first) == inliers.end()) continue;
     sum_diff2 += diff.dot(diff);
     ++point_num;
   }
--- g2o/examples/ba_anchored_inverse_depth/ba_anchored_inverse_depth_demo.cpp
+++ g2o/examples/ba_anchored_inverse_depth/ba_anchored_inverse_depth_demo.cpp
@@ -24,16 +24,15 @@
 // NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 // SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
+#include <cstdint>
 #include <iostream>
-#include <stdint.h>
-
 #include <unordered_set>
 
-#include "g2o/core/sparse_optimizer.h"
 #include "g2o/core/block_solver.h"
-#include "g2o/core/solver.h"
-#include "g2o/core/robust_kernel_impl.h"
 #include "g2o/core/optimization_algorithm_levenberg.h"
+#include "g2o/core/robust_kernel_impl.h"
+#include "g2o/core/solver.h"
+#include "g2o/core/sparse_optimizer.h"
 #include "g2o/solvers/dense/linear_solver_dense.h"
 #include "g2o/types/sba/types_six_dof_expmap.h"
 //#include "g2o/math_groups/se3quat.h"
@@ -46,30 +45,24 @@
 #include "g2o/solvers/eigen/linear_solver_eigen.h"
 #endif
 
-using namespace Eigen;
-using namespace std;
+using std::cout;
+using std::cerr;
+using std::endl;
 
 class Sample {
  public:
   static int uniform(int from, int to) { return static_cast<int>(g2o::Sampler::uniformRand(from, to)); }
 };
 
-Vector2d project2d(const Vector3d& v){
-  Vector2d res;
-  res(0) = v(0)/v(2);
-  res(1) = v(1)/v(2);
-  return res;
+g2o::Vector2 project2d(const g2o::Vector3& v){
+  return g2o::Vector2(v(0)/v(2), v(1)/v(2));
 }
 
-Vector3d unproject2d(const Vector2d& v){
-  Vector3d res;
-  res(0) = v(0);
-  res(1) = v(1);
-  res(2) = 1;
-  return res;
+g2o::Vector3 unproject2d(const g2o::Vector2& v){
+  return g2o::Vector3(v(0), v(1), 1.);
 }
 
-inline Vector3d invert_depth(const Vector3d & x){
+inline g2o::Vector3 invert_depth(const g2o::Vector3 & x){
   return unproject2d(x.head<2>())/x[2];
 }
 
@@ -136,17 +129,17 @@ int main(int argc, const char* argv[]){
 
   optimizer.setAlgorithm(std::unique_ptr<g2o::OptimizationAlgorithm>(solver.release()));
 
-  vector<Vector3d> true_points;
+  std::vector<g2o::Vector3> true_points;
   for (size_t i=0;i<500; ++i){
-    true_points.push_back(Vector3d((g2o::Sampler::uniformRand(0., 1.)-0.5)*3,
+    true_points.emplace_back((g2o::Sampler::uniformRand(0., 1.)-0.5)*3,
                                    g2o::Sampler::uniformRand(0., 1.)-0.5,
-                                   g2o::Sampler::uniformRand(0., 1.)+3));
+                                   g2o::Sampler::uniformRand(0., 1.)+3);
   }
 
   double focal_length= 1000.;
-  Vector2d principal_point(320., 240.);
+  g2o::Vector2 principal_point(320., 240.);
 
-  vector<g2o::SE3Quat, aligned_allocator<g2o::SE3Quat>> true_poses;
+  std::vector<g2o::SE3Quat, Eigen::aligned_allocator<g2o::SE3Quat>> true_poses;
 
   auto cam_params = std::make_shared<g2o::CameraParameters>(focal_length, principal_point, 0.);
   cam_params->setId(0);
@@ -157,7 +150,7 @@ int main(int argc, const char* argv[]){
 
   int vertex_id = 0;
   for (size_t i=0; i<15; ++i){
-    Vector3d trans(i*0.04-1.,0,0);
+    g2o::Vector3 trans(i*0.04-1.,0,0);
     Eigen:: Quaterniond q;
     q.setIdentity();
     g2o::SE3Quat T_me_from_world(q,trans);
@@ -178,15 +171,15 @@ int main(int argc, const char* argv[]){
   double sum_diff2 = 0;
 
   cout << endl;
-  unordered_map<int,int> pointid_2_trueid;
-  unordered_map<int,int> pointid_2_anchorid;
-  unordered_set<int> inliers;
+  std::unordered_map<int,int> pointid_2_trueid;
+  std::unordered_map<int,int> pointid_2_anchorid;
+  std::unordered_set<int> inliers;
 
   for (size_t i=0; i<true_points.size(); ++i){
     auto v_p = std::make_shared<g2o::VertexPointXYZ>();
     int num_obs = 0;
-    for (size_t j=0; j<true_poses.size(); ++j)    {
-      Vector2d z = cam_params->cam_map(true_poses.at(j).map(true_points.at(i)));
+    for (auto & true_pose : true_poses)    {
+      g2o::Vector2 z = cam_params->cam_map(true_pose.map(true_points.at(i)));
       if (z[0] >= 0 && z[1] >= 0 && z[0] < 640 && z[1] < 480) {
         ++num_obs;
       }
@@ -196,34 +189,34 @@ int main(int argc, const char* argv[]){
       int anchor_frame_id = -1;
       bool inlier = true;
       for (size_t j=0; j<true_poses.size(); ++j){
-        Vector2d z = cam_params->cam_map(true_poses.at(j).map(true_points.at(i)));
+        g2o::Vector2 z = cam_params->cam_map(true_poses.at(j).map(true_points.at(i)));
 
         if (z[0]>=0 && z[1]>=0 && z[0]<640 && z[1]<480)        {
           if (anchor_frame_id==-1){
             anchor_frame_id = j;
-            pointid_2_anchorid.insert(make_pair(point_id, anchor_frame_id));
+            pointid_2_anchorid.insert(std::make_pair(point_id, anchor_frame_id));
             g2o::SE3Quat T_anchor_from_world = true_poses[anchor_frame_id];
             v_p->setId(point_id);
             if (SCHUR_TRICK){
               v_p->setMarginalized(true);
             }
-            Vector3d point_w = true_points.at(i)
-                + Vector3d(g2o::Sampler::gaussRand(0., 1),
+            g2o::Vector3 point_w = true_points.at(i)
+                + g2o::Vector3(g2o::Sampler::gaussRand(0., 1),
                            g2o::Sampler::gaussRand(0., 1),
                            g2o::Sampler::gaussRand(0., 1));
 
-            Vector3d point_anchor = T_anchor_from_world*point_w;
+            g2o::Vector3 point_anchor = T_anchor_from_world*point_w;
             v_p->setEstimate(invert_depth(point_anchor));
             optimizer.addVertex(v_p);
           }
 
           double sam = g2o::Sampler::uniformRand(0., 1.);
           if (sam<OUTLIER_RATIO){
-            z = Vector2d(Sample::uniform(0,640),
+            z = g2o::Vector2(Sample::uniform(0,640),
                          Sample::uniform(0,480));
             inlier= false;
           }
-          z += Vector2d(g2o::Sampler::gaussRand(0., PIXEL_NOISE),
+          z += g2o::Vector2(g2o::Sampler::gaussRand(0., PIXEL_NOISE),
                         g2o::Sampler::gaussRand(0., PIXEL_NOISE));
           auto e = std::make_shared<g2o::EdgeProjectPSI2UV>();
 
@@ -233,7 +226,7 @@ int main(int argc, const char* argv[]){
           e->setVertex(2, optimizer.vertices().find(anchor_frame_id)->second);
 
           e->setMeasurement(z);
-          e->information() = Matrix2d::Identity();
+          e->information() = g2o::Matrix2::Identity();
 
           if (ROBUST_KERNEL) {
             e->setRobustKernel(std::make_shared<g2o::RobustKernelHuber>());
@@ -245,10 +238,10 @@ int main(int argc, const char* argv[]){
       }
       if (inlier){
         inliers.insert(point_id);
-        Vector3d diff = true_poses[anchor_frame_id].inverse() * invert_depth(v_p->estimate()) - true_points[i];
+        g2o::Vector3 diff = true_poses[anchor_frame_id].inverse() * invert_depth(v_p->estimate()) - true_points[i];
         sum_diff2 += diff.dot(diff);
       }
-      pointid_2_trueid.insert(make_pair(point_id,i));
+      pointid_2_trueid.insert(std::make_pair(point_id,i));
       ++point_id;
       ++point_num;
     }
@@ -265,31 +258,30 @@ int main(int argc, const char* argv[]){
   cout << "Point error before optimisation (inliers only): " << sqrt(sum_diff2/point_num) << endl;
   point_num = 0;
   sum_diff2 = 0;
-  for (unordered_map<int,int>::iterator it=pointid_2_trueid.begin();
-       it!=pointid_2_trueid.end(); ++it){
-    g2o::HyperGraph::VertexIDMap::iterator v_it = optimizer.vertices().find(it->first);
+  for (auto & it : pointid_2_trueid){
+    auto v_it = optimizer.vertices().find(it.first);
     if (v_it==optimizer.vertices().end()){
-      cerr << "Vertex " << it->first << " not in graph!" << endl;
+      cerr << "Vertex " << it.first << " not in graph!" << endl;
       exit(-1);
     }
-    g2o::VertexPointXYZ* v_p = dynamic_cast<g2o::VertexPointXYZ*>(v_it->second.get());
-    if (v_p==0){
-      cerr << "Vertex " << it->first << "is not a PointXYZ!" << endl;
+    auto* v_p = dynamic_cast<g2o::VertexPointXYZ*>(v_it->second.get());
+    if (v_p==nullptr){
+      cerr << "Vertex " << it.first << "is not a PointXYZ!" << endl;
       exit(-1);
     }
-    int anchorframe_id = pointid_2_anchorid.find(it->first)->second;
+    int anchorframe_id = pointid_2_anchorid.find(it.first)->second;
     v_it = optimizer.vertices().find(anchorframe_id);
     if (v_it==optimizer.vertices().end()){
-      cerr << "Vertex " << it->first << " not in graph!" << endl;
+      cerr << "Vertex " << it.first << " not in graph!" << endl;
       exit(-1);
     }
-    g2o::VertexSE3Expmap* v_anchor = dynamic_cast<g2o::VertexSE3Expmap*>(v_it->second.get());
-    if (v_anchor==0){
-      cerr << "Vertex " << it->second << "is not a SE3Expmap!" << endl;
+    auto* v_anchor = dynamic_cast<g2o::VertexSE3Expmap*>(v_it->second.get());
+    if (v_anchor==nullptr){
+      cerr << "Vertex " << it.second << "is not a SE3Expmap!" << endl;
       exit(-1);
     }
-    Vector3d diff = v_anchor->estimate().inverse()*invert_depth(v_p->estimate())-true_points[it->second];
-    if (inliers.find(it->first)==inliers.end())
+    g2o::Vector3 diff = v_anchor->estimate().inverse()*invert_depth(v_p->estimate())-true_points[it.second];
+    if (inliers.find(it.first)==inliers.end())
       continue;
     sum_diff2 += diff.dot(diff);
     ++point_num;
