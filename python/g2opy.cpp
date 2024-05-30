06492d4ecfcbfb02d426d81b1ab2519c0013717a
Rainer Kuemmerle <rainer.kuemmerle@gmail.com>
2024-05-30 18:48:43 +0200

    Add python wrapper for simulator 2D

--- python/g2opy.cpp
+++ python/g2opy.cpp
@@ -1,13 +1,19 @@
-#include "g2opy.h"
-
 #include "python/core/py_core.h"
 #include "python/types/py_types.h"
+
+#ifdef HAVE_G2O_SIMULATOR
+#include "python/simulator/py_simulator.h"
+#endif
 
 namespace g2o {
 
 PYBIND11_MODULE(g2opy, m) {
   declareCore(m);
   declareTypes(m);
+
+#ifdef HAVE_G2O_SIMULATOR
+  declareSimulator(m);
+#endif
 }
 
 }  // namespace g2o
